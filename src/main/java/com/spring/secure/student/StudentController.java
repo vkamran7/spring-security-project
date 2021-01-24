9642653e70eaffcba3044a16ed787300c6c71097
Kamran <vkamran7@gmail.com>
2021-01-24 21:52:50 +0400

    basic spring project without security

---
+++ src/main/java/com/spring/secure/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.spring.secure.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Kamran Valizada"),
+            new Student(2, "Mark Hamill"),
+            new Student(3, "Spider-Man")
+    );
+
+    @GetMapping(path = "/{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student " + studentId + " does not exist"));
+    }
+}
